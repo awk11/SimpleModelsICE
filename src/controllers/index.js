c335559918b8d2cef8d7e5d5646998bed5adfb88
awk11 <awk9293@ad.rit.edu>
2016-03-16 13:50:33 -0400

    maybe?

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -209,7 +209,7 @@ var setDogName = function(req, res) {
     
     //check if the required fields exist
     //normally you would also perform validation to know if the data they sent you was real 
-    if(!req.body.firstname || !req.body.lastname || !req.body.beds) {
+    if(!req.body.name || !req.body.breed || !req.body.age) {
         //if not respond with a 400 error (either through json or a web page depending on the client dev)
         return res.status(400).json({error: "name,breed and age are all required"});
     }
